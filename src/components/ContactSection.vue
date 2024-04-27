6f4a09352cc50c0f89f0976f5b00c057e761f15a
hiyorun <hiyorrun@e.email>
2024-04-27 16:53:56 +0700

    chore: svelte boilerplate

--- src/components/ContactSection.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import Form from '../components/Form.vue';
-</script>
-<template>
-    <div class="bg-white m-auto py-24 flex flex-col items-center">
-        <h3 class="text-4xl text-black font-title py-12">Say Hi, Don't be Shy</h3>
-        <Form />
-    </div>
-</template>
