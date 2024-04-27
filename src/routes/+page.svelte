f63789da2100f5c5a47723a1fbfa55cd4f06512a
hiyorun <hiyorrun@e.email>
2024-04-27 19:24:51 +0700

    feat: migrated Contact section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Hero from './Hero.svelte';
 	import Projects from './Projects.svelte';
+    import Contact from './Contact.svelte';
 </script>
 
 <div class="w-full h-full bg-black text-white">
 	<Hero />
 	<Projects />
+    <Contact/>
 </div>
