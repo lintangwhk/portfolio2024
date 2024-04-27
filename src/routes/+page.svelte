e5deba44492f7121b793018f7a746e11e08d4ade
hiyorun <hiyorrun@e.email>
2024-04-27 19:08:35 +0700

    feat: migrated Hero section

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Hero from './Hero.svelte';
+</script>
+
+<div class="w-full h-full bg-black text-white">
+	<Hero />
+</div>
---
+++ src/routes/Hero.svelte
@@ -0,0 +1,5 @@
+<div class="py-6">
+    <video controls autoplay muted class="w-full h-fit m-auto">
+        <source src="/src/assets/profile.webm" type="video/webm" />
+    </video>
+</div>
