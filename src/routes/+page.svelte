543f0c4db7f3567bebbc4ed3c1e0555bf04dbc9f
hiyorun <hiyorrun@e.email>
2024-04-27 19:09:04 +0700

    feat: migrated Projects section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Hero from './Hero.svelte';
+	import Projects from './Projects.svelte';
 </script>
 
 <div class="w-full h-full bg-black text-white">
 	<Hero />
+	<Projects />
 </div>
---
+++ src/routes/Projects.svelte
@@ -0,0 +1,28 @@
+<script>
+	const projects = [
+		{
+			title: 'Graphic Design and Illustrations',
+			image: '/src/assets/sample.png'
+		},
+		{
+			title: 'Creative Direction'
+		},
+		{
+			title: 'UI and UX Design'
+		}
+	];
+</script>
+
+<div class="w-full p-6 m-auto max-w-7xl">
+	<h3 class="text-4xl font-title py-12">Projects That I Worked On</h3>
+	<div class="flex flex-col md:flex-row gap-5 justify-center items-center md:h-[80vh]">
+		{#each projects as project}
+			<div
+				class="flex flex-col-reverse h-96 md:h-full w-2/3 md:w-2/6 border border-white p-4"
+				style="background-image: url({project.image}); background-size: cover; background-repeat: no-repeat; background-position: center"
+			>
+				<button class="h-20 bg-black">{project.title}</button>
+			</div>
+		{/each}
+	</div>
+</div>
